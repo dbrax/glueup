5f5bf8668a794accc85b640fb859b9e53bb8f79b
Emmanuel <epmnzava@gmail.com>
2022-09-18 21:19:30 +0300

    updated code

--- src/User.php
+++ src/User.php
@@ -5,6 +5,10 @@ namespace Epmnzava\Glueup;
 class User
 {
 
+
+
+
+
     public function __construct()
     {
     }
@@ -38,5 +42,28 @@ class User
         );
 
         $result = Glueup::put('subscription/subscribeList', $params);
+
+        return $result;
+    }
+
+
+    public function loginUser($email, $password)
+    {
+        $params = array(
+            'email'      => array('value' => $email),
+            'passphrase' => array('value' => md5($password))
+        );
+
+        $result = GlueUp::post('user/session', $params);
+
+        return $result;
+    }
+    public function keepUserLogginedIn($token,)
+    {
+        $redirect_url = '/my/profile/';
+        $timestamp    = time();
+        $result = Glueup::get('user/redirect/token?new_broker=php_' . Glueup::$API_account . '&timestamp=' . $timestamp . '&redirect_url=' . $redirect_url, $token);
+
+        return $result;
     }
 }
